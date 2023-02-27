8c5987cb27521776108c6749484d35c6ef2a050e
DevTesaG <feynman.stories@gmail.com>
2023-02-26 19:56:45 -0600

    router fixed take 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { LandingComponent } from './pages/landing/landing.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent},
-  { path: '', redirectTo: '', pathMatch: 'full'},
-  { path: '**', redirectTo: '', pathMatch: 'full'},
   { path: 'sells', loadChildren: () => import('./sells/sells.module').then(m => m.SellsModule) },
   { path: 'inventory', loadChildren: () => import('./inventory/inventory.module').then(m => m.InventoryModule)},
   { path: 'history', loadChildren: () => import('./history/history.module').then(m => m.HistoryModule)}
