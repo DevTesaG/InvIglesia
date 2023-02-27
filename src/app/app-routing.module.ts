18dc97ed6464ddfe99148cc17a3bd2db4abbc258
DevTesaG <feynman.stories@gmail.com>
2023-02-26 19:32:51 -0600

    router fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent},
   { path: '', redirectTo: '', pathMatch: 'full'},
   { path: '**', redirectTo: '', pathMatch: 'full'},
-  // { path: 'sells', loadChildren: () => import('./sells/sells.module').then(m => m.SellsModule) },
-  // { path: 'inventory', loadChildren: () => import('./inventory/inventory.module').then(m => m.InventoryModule)},
-  // { path: 'history', loadChildren: () => import('./history/history.module').then(m => m.HistoryModule)}
   { path: 'sells', loadChildren: () => import('./sells/sells.module').then(m => m.SellsModule) },
-  { path: '/InvIglesia/inventory', loadChildren: () => import('./inventory/inventory.module').then(m => m.InventoryModule)},
+  { path: 'inventory', loadChildren: () => import('./inventory/inventory.module').then(m => m.InventoryModule)},
   { path: 'history', loadChildren: () => import('./history/history.module').then(m => m.HistoryModule)}
 ];
 
 
 @NgModule({
-  imports: [   RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules,
-  }),],
+  imports: [   RouterModule.forRoot(routes),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
